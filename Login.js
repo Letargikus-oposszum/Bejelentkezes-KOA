e506903f016c7f99b1af33850bc5722586d5a1b6
Letargikus-oposszum <b1vidtam@gmail.com>
2025-09-12 10:16:49 +0200

    basic html and js done

---
+++ Login.js
@@ -0,0 +1,16 @@
+document.getElementById("loginbtn").addEventListener("click", function() {
+    document.getElementById("login").style.display = "block";
+    document.getElementById("registration").style.display = "none";
+});
+document.getElementById("registrationbtn").addEventListener("click", function() {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("registration").style.display = "block";
+});
+
+document.getElementById("loginSubmit").addEventListener("click", function(){
+    
+});
+
+document.getElementById("registrationSubmit").addEventListener("click", function(){
+
+});
